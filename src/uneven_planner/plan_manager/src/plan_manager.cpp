4255266594cd22815a101cf304eef4bf5ac3a972
Runqi Qiu <runqi_qiu@outlook.com>
2024-01-03 23:26:23 +0800

    现在是加了一个读取 csv 参考点 & 将结果保存为 csv 的功能；很简陋，顶多是用于后面的论文可视化，为了后续工作考虑，还是得将接口对齐一下，能接受参考点输入，然后给出路径输出。

--- src/uneven_planner/plan_manager/src/plan_manager.cpp
+++ src/uneven_planner/plan_manager/src/plan_manager.cpp
@@ -1,7 +1,85 @@
 #include "plan_manager/plan_manager.h"
 
+
 namespace uneven_planner
 {
+    /**
+     * @brief 从本地的 .csv 文件中读取参考路径，作为后续规划的初值
+     * 
+     * @param file_path 本地文件的目录
+     * @return std::vector<Eigen::Vector3d> 
+     */
+    std::vector<Eigen::Vector3d> readPathFromCSV(const std::string& file_path) {
+        std::vector<Eigen::Vector2d> path_xy;
+        std::ifstream file(file_path);
+        std::string line;
+
+        // 跳过注释
+        std::getline(file, line);
+        while (std::getline(file, line)) {
+            if (line.empty() || line[0] == '#') 
+            {
+                continue;
+            }
+
+            std::stringstream line_stream(line);
+            std::string cell;
+            std::vector<double> point_data;
+            while (std::getline(line_stream, cell, ',')) 
+            {
+                point_data.push_back(std::stod(cell));
+            }
+        
+            Eigen::Vector2d point(-point_data[0], -point_data[1]);
+            path_xy.push_back(point);
+        }
+        
+        std::vector<Eigen::Vector3d> path;
+
+        // Compute heading for the first point using forward difference
+        Eigen::Vector2d forward_diff = path_xy[1] - path_xy[0];
+        Eigen::Vector3d start_point(path_xy[0].x(), path_xy[0].y(), std::atan2(forward_diff.y(), forward_diff.x()));
+        path.push_back(start_point);
+
+        // Compute heading for points in the middle using central differences
+        for (size_t i = 1; i < path_xy.size() - 1; ++i) {
+            Eigen::Vector2d diff = path_xy[i + 1] - path_xy[i - 1];
+            Eigen::Vector3d temp_point(path_xy[i].x(), path_xy[i].y(), std::atan2(diff.y(), diff.x()));
+            path.push_back(temp_point);
+        }
+
+        // Compute heading for the last point using backward difference
+        Eigen::Vector2d backward_diff = path_xy[path_xy.size() - 1] - path_xy[path_xy.size() - 2];
+        Eigen::Vector3d end_point(path_xy[path_xy.size() - 1].x(), path_xy[path_xy.size() - 1].y(), std::atan2(backward_diff.y(), backward_diff.x()));
+        path.push_back(end_point);
+
+        return path;
+    }
+
+    /**
+     * @brief 将规划结果保存到本地
+     * 
+     * @param result_path 规划结果
+     * @param file_path   保存文件的目录
+     * @return true 
+     * @return false 
+     */
+    bool savePathToCSV(const std::vector<Eigen::Vector2d>& result_path, const std::string& file_path) {
+        std::ofstream file(file_path);
+        
+        if (!file.is_open()) {
+            std::cerr << "Error: Could not open file " << file_path << std::endl;
+            return false;
+        }
+
+        for (const Eigen::Vector2d& point : result_path) {
+            file << point.x() << "," << point.y() << "\n";
+        }
+
+        file.close();
+        return true;
+    }
+
     void PlanManager::init(ros::NodeHandle& nh)
     {
         nh.getParam("manager/piece_len", piece_len);
@@ -68,7 +146,22 @@ namespace uneven_planner
                                   atan2(2.0*msg.pose.orientation.z*msg.pose.orientation.w, \
                                         2.0*pow(msg.pose.orientation.w, 2)-1.0)             );
         
-        std::vector<Eigen::Vector3d> init_path = kino_astar->plan(odom_pos, end_state);
+        // std::vector<Eigen::Vector3d> init_path = kino_astar->plan(odom_pos, end_state);
+        
+        // -------------------------------------------------------------------------------------
+        // 替换利用 knio_astar 求解参考路径 init_path = kino_astar->plan(odom_pos, end_state) 
+        // 转而从 CSV 文件中读取参考线和起点 & 终点位姿
+        // -------------------------------------------------------------------------------------
+        std::vector<Eigen::Vector3d> init_path = readPathFromCSV(
+            "/home/qrq_18/uneven_planner/src/src/uneven_planner/plan_manager/data/reference.csv");
+        
+        std::cout << "GO GO GO!" << std::endl;
+        for (const auto& pt : init_path) {
+            std::cout << "[" << pt.x() << ", " << pt.y() << ", " << pt.z() << "]" << std::endl;
+        }
+
+        // -------------------------------------------------------------------------------------
+        
         if (init_path.empty())
         {
             in_plan = false;
@@ -101,8 +194,12 @@ namespace uneven_planner
     
         init_xy << init_path[0].x(), 0.0, 0.0, \
                    init_path[0].y(), 0.0, 0.0;
-        end_xy << init_path.back().x(), 0.0, 0.0, \
-                   init_path.back().y(), 0.0, 0.0;
+        // end_xy << init_path.back().x(), 0.0, 0.0, \
+        //            init_path.back().y(), 0.0, 0.0;
+
+        end_xy << -19.495  , 0.0, 0.0, \
+                  0.444386, 0.0, 0.0;        
+
         init_yaw << init_path[0].z(), 0.0, 0.0;
         end_yaw << init_path.back().z(), 0.0, 0.0;
 
@@ -163,7 +260,7 @@ namespace uneven_planner
         std::cout << "min cosxi:   "<< -max_terrain_value[4] << std::endl;
         std::cout << "max sigma:   "<< max_terrain_value[5] << std::endl;
 
-        // publish to mpc controller
+        // publish to mpc controller and save result to csv
         mpc_controller::SE2Traj traj_msg;
         traj_msg.start_time = ros::Time::now();
         traj_msg.init_v.x = 0.0;
@@ -181,6 +278,22 @@ namespace uneven_planner
             traj_msg.pos_pts.push_back(pospt);
             traj_msg.posT_pts.push_back(back_end_traj.pos_traj[i].getDuration());
         }
+
+        // save result to csv
+        std::vector<Eigen::Vector2d> result_path;
+
+        for (double t = 0.0; t < back_end_traj.getTotalDuration(); t += 0.03)
+        {
+            Eigen::Vector2d pos = back_end_traj.getPos(t);
+            Eigen::Vector2d path_point(pos(0), pos(1));
+            result_path.push_back(path_point);
+        }
+
+        bool save_flag = savePathToCSV(result_path, "/home/qrq_18/uneven_planner/src/src/uneven_planner/plan_manager/data/uneven_result.csv");
+        if (save_flag) {
+            std::cout << "\nOhhhhhh Yeah!\n" << std::endl;
+        }
+
         geometry_msgs::Point pospt;
         Eigen::Vector2d pos = back_end_traj.pos_traj.getValue(back_end_traj.pos_traj.getTotalDuration());
         pospt.x = pos[0];
@@ -200,7 +313,7 @@ namespace uneven_planner
         traj_msg.angle_pts.push_back(anglept);
         traj_pub.publish(traj_msg);
         in_plan = false;
-
+        
         return;
     }
 }
